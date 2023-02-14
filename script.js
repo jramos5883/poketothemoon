621863536dc3dd82c6d2a85bbc6d587c7e509b7e
John Ramos <jramos5883@gmail.com>
2023-02-13 16:58:07 -0800

    Finished location page layout, need to work on the small details, font and layout.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Comment
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
+  const uluru = { lat: 33.8261, lng: -117.8378 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 15,
     center: uluru,
   });
   // The marker, positioned at Uluru
