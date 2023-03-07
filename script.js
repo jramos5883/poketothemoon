6b67a7aeebbecceb79131de0414fd5781e8db094
John Ramos <jramos5883@gmail.com>
2023-03-07 06:51:27 -0800

    Quick check of resturant page for poke place, may change fonts for menu page, still need to connect social media to buttons. Honest, when I finish with my react course, I want to return to this project and build it again in react.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Comment
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: 33.8261, lng: -117.8378 };
+  const pokeToTheMoon = { lat: 33.8261, lng: -117.8378 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
-    center: uluru,
+    zoom: 14,
+    center: pokeToTheMoon,
   });
   // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: pokeToTheMoon,
     map: map,
   });
 }
